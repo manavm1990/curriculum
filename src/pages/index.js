d1a89d5d00aef5aaa3b92c5277f29580a70d08ec
Manav Misra <manavm@visionwebsoft.com>
2024-08-11 11:13:24 -0500

    style: :art:

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import HomepageFeatures from "@site/src/components/Homepage";
+import Heading from "@theme/Heading";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
-
-import Heading from "@theme/Heading";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
